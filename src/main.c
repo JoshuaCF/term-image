77c1885589d6bf3c43511ffb16835642506beeeb
Joshua Flowers <flowers.joshuac@gmail.com>
2024-08-18 01:11:00 -0500

    File is now specified at the command line

--- src/main.c
+++ src/main.c
@@ -4,9 +4,14 @@
 #include "img.h"
 #include "ppm.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("Provide a single .ppm file as an argument\n");
+		return 1;
+	}
+
 	struct Image testImage;
-	int status = loadPPM("resources/ColorHead.ppm", &testImage);
+	int status = loadPPM(argv[1], &testImage);
 	if (status != 0) return status;
 
 	drawImage(testImage);
