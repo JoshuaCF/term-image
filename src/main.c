ff28e14a04e5d240afd7a9d82f94b33edf1f333f
Joshua Flowers <flowers.joshuac@gmail.com>
2024-08-17 23:35:45 -0500

    Made image drawing square

--- src/main.c
+++ src/main.c
@@ -14,19 +14,26 @@ void freeImage(struct Image* img) {
 }
 void drawImage(struct Image img) {
 	printf("\n");
-	for (unsigned int y = 0; y < img.height; y++) {
+	for (unsigned int y = 0; y < img.height; y += 2) {
 		for (unsigned int x = 0; x < img.width; x++) {
-			struct Pixel curPixel = img.data[y*img.width + x];
-			printf("\e[38;2;%hhu;%hhu;%hhum█", curPixel.r, curPixel.g, curPixel.b);
+			printf("\e[0m"); // reset
+			struct Pixel curPixelTop = img.data[y*img.width + x];
+			printf("\e[38;2;%hhu;%hhu;%hhum", curPixelTop.r, curPixelTop.g, curPixelTop.b);
+			
+			if (y+1 < img.height) {
+				struct Pixel curPixelBottom = img.data[(y+1)*img.width + x];
+				printf("\e[48;2;%hhu;%hhu;%hhum", curPixelBottom.r, curPixelBottom.g, curPixelBottom.b);
+			}
+			printf("▀");
 		}
-		printf("\n");
+		printf("\e[0m\n");
 	}
 }
 
 int main() {
 	struct Image testImage = {};
-	testImage.width = 10;
-	testImage.height = 10;
+	testImage.width = 9;
+	testImage.height = 9;
 	struct Pixel* imageData = malloc(testImage.width * testImage.height * sizeof(struct Pixel));
 	testImage.data = imageData;
 
